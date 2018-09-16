76b7e4942a103e7cca8c8ffa006202632ac06f9e
parkchanjong <7000cj@gmail.com>
2018-09-17 02:40:01 +0900

    이미지 업로더2

--- fooddiary/app/controllers/comments_controller.rb
+++ fooddiary/app/controllers/comments_controller.rb
@@ -16,6 +16,6 @@ class CommentsController < ApplicationController
      
       private
         def comment_params
-          params.require(:comment).permit(:calorie, :time)
+          params.require(:comment).permit(:calorie, :time, :day ,:bld)
         end
 end
--- fooddiary/app/models/diary.rb
+++ fooddiary/app/models/diary.rb
@@ -1,4 +1,5 @@
 class Diary < ApplicationRecord
+    mount_uploader :image, ImageUploader
     has_many :comments, dependent: :destroy
     validates :foodname, presence: true,
                     length: { minimum: 1 }
